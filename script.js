493dcb3369af95f11c82c2b69e8baf4801468ef5
Anna Jeziorska <patryk.napiorkowski95@gmail.com>
2023-08-03 18:48:14 +0200

    added sub menu for offer and some animation to this

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
 // Pobranie przycisków z oferty 
-const ppozBtn = document.getElementById('btn-ppoz');
-const atexBtn = document.getElementById('btn-atex');
-const sevesoBtn = document.getElementById('btn-seveso');
-const trainingBtn = document.getElementById('btn-training');
+const ppozBtn = document.getElementById('ppoz');
+const atexBtn = document.getElementById('atex');
+const sevesoBtn = document.getElementById('seveso');
+const trainingBtn = document.getElementById('training');
+
+//zmienne do zmiany oferty
+const photoContainer = document.getElementById('photo-container');
+
+//tablica do przycisków 
+const subMenu = document.querySelector('.sub-menu');
+const offerArea = document.querySelector('.main-offer-info__container');
+const ppozButtonsArray = ['Instrukcja bezpieczeństwa pożarowego', 'Operat przeciwpożarowy', 'Ekspertyza ppoż', 'Uzgadnianie projektów architektoniczno-budowlanych', 'Nadzór ppoż. nad bezpieczeństwem pożarowym', 'Instrukcja bezpieczeństwa pożarowego'];
+const atexButtonsArray = ['Ocena zagrożenia wybuchem', 'Ocena Ryzyka Wybuchu', 'Dokument zabezpieczenia przed wybuchem'];
+const sevesoButtonsArray = ['Dokumentacja SEVESO', 'Raport o Bezpieczeństwie', 'Wewnętrzny Plan Operacyjno-Ratowniczy', 'Program Zapobiegania Awariom'];
+const trainingButtonsArray = ['Szkolenia PPOŻ', 'Szkolenia BHP'];
+
+const renderButtons = (offerId, buttonsArray) => {
+    subMenu.innerHTML = '';
+    for(const button of buttonsArray) {
+        const buttonEl = document.createElement('button');
+        buttonEl.textContent = button;
+        buttonEl.setAttribute('id', `${offerId}-${button.id}`);
+        buttonEl.classList = 'sub-menu-btn';
+        subMenu.append(buttonEl);
+    }
+}
+
+const changePhoto = (offerId) => {
+    if(offerId === 'atex') {
+        photoContainer.classList = '';
+
+        photoContainer.className = 'atex-active';
+        renderButtons(offerId, atexButtonsArray);
+
+    } else if (offerId === 'seveso') {
+        photoContainer.classList = '';
+
+        photoContainer.className = 'seveso-active';
+        renderButtons(offerId, sevesoButtonsArray);
+
+    } else if (offerId === 'ppoz') { 
+        photoContainer.classList = '';
+
+        photoContainer.className = 'ppoz-active';
+        renderButtons(offerId, ppozButtonsArray);
+        
+    } else {
+        photoContainer.classList = '';
+
+        photoContainer.className = 'training-active';
+        renderButtons(offerId, trainingButtonsArray);
+
+    }
+}
 
 const highlitedOffer = (btn) => {
     ppozBtn.className = '';
@@ -12,8 +62,11 @@ const highlitedOffer = (btn) => {
     btn.className = 'active';
 }
 
+
 function handleClick(event) {
     const btnId = event.target.id;
+    console.log(btnId);
+    changePhoto(btnId);
     highlitedOffer(event.target);
 }
 
