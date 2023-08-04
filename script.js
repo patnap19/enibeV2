c9e68e96930561bb2c9ec4a1400454c2da49b9c1
Anna Jeziorska <patryk.napiorkowski95@gmail.com>
2023-08-04 10:29:17 +0200

    added click event to show modal in ppoz and atex

--- script.js
+++ script.js
@@ -3,51 +3,225 @@ const ppozBtn = document.getElementById('ppoz');
 const atexBtn = document.getElementById('atex');
 const sevesoBtn = document.getElementById('seveso');
 const trainingBtn = document.getElementById('training');
-
+const modal = document.querySelector('.modal-shadow');
+const modalContainer = modal.querySelector('.modal');
 //zmienne do zmiany oferty
 const photoContainer = document.getElementById('photo-container');
 
 //tablica do przycisków 
 const subMenu = document.querySelector('.sub-menu');
 const offerArea = document.querySelector('.main-offer-info__container');
-const ppozButtonsArray = ['Instrukcja bezpieczeństwa pożarowego', 'Operat przeciwpożarowy', 'Ekspertyza ppoż', 'Uzgadnianie projektów architektoniczno-budowlanych', 'Nadzór ppoż. nad bezpieczeństwem pożarowym', 'Instrukcja bezpieczeństwa pożarowego'];
+const ppozButtonsArray = ['Instrukcja bezpieczeństwa pożarowego', 'Operat przeciwpożarowy', 'Ekspertyza ppoż', 'Uzgadnianie projektów architektoniczno-budowlanych', 'Nadzór ppoż. nad bezpieczeństwem pożarowym'];
 const atexButtonsArray = ['Ocena zagrożenia wybuchem', 'Ocena Ryzyka Wybuchu', 'Dokument zabezpieczenia przed wybuchem'];
 const sevesoButtonsArray = ['Dokumentacja SEVESO', 'Raport o Bezpieczeństwie', 'Wewnętrzny Plan Operacyjno-Ratowniczy', 'Program Zapobiegania Awariom'];
 const trainingButtonsArray = ['Szkolenia PPOŻ', 'Szkolenia BHP'];
 
+//tablice z obiektami do opisów
+
+const descPpozArray = [
+    {
+        flag: 'ppoz-0',
+        title: 'Instrukcja bezpieczeństwa pożarowego',
+        desc: `Instrukcja Bezpieczeństwa Pożarowego odgrywa istotną rolę w zapewnianiu odpowiedniego
+        poziomu bezpieczeństwa w miejscach pracy oraz obiektach użyteczności publicznej.
+        Opracowanie instrukcji
+        Instrukcja Bezpieczeństwa Pożarowego jest opracowywana na postawie materiałów udostępnionych
+        przez Klienta, wizji lokalnej obiektu, zakładu oraz obecnie obowiązujących przepisów
+        przeciwpożarowych.
+        Co powinna zawierać instrukcja bezpieczeństwa pożarowego:
+        • część opisowa – opracowana zgodnie z § 6. 1 Rozporządzenia MSWiA z dnia 7 czerwca 2010 r. w
+        sprawie ochrony przeciwpożarowej budynków, innych obiektów budowlanych i terenów (Dz. U. Nr
+        109, Poz. 719).
+        • część graficzna – zawiera informacje na temat rozmieszczenia sprzętu gaśniczego, urządzeń
+        przeciwpożarowych, znaków ewakuacyjnych, planu ewakuacji, planu usytuowania obiektu/ów na
+        działce itd.
+        • załączniki – np. oświadczenie pracownika o zapoznaniu się z instrukcją, karty charakterystyki
+        substancji niebezpiecznych i innych według wymagań.`
+    }, 
+    {
+        flag: 'ppoz-1',
+        title: 'Operat przeciwpożarowy',
+        desc: `Obowiązek opracowania Operatu Przeciwpożarowego wynika z art. 42 ust. 4b pkt 1 Ustawy
+        z dnia 14 grudnia 2012 r. o odpadach (Dz.U.2022.0.699).
+        Operat Przeciwpożarowy zawiera warunki ochrony przeciwpożarowej instalacji, obiektu lub jego
+        części lub innego miejsca magazynowania odpadów, uzgodnione z komendantem powiatowym
+        (miejskim) Państwowej Straży Pożarnej.`
+    },
+    {
+        flag: 'ppoz-2',
+        title: 'Ekspertyza ppoż',
+        desc: `Zgodnie z art. 4 Ustawy z dnia 24 sierpnia 1991 r. o ochronie przeciwpożarowej za bezpieczeństwo
+        pożarowe w budynku odpowiada jego właściciel, zarządca bądź użytkownik .
+        Espertyza stanu ochrony przeciwpożarowej, zwana jest dokumentem zawierającym rozwiązania
+        zamienne w stosunku do wymagań określonych w przepisach dotyczących ochrony
+        przeciwpożarowej.
+        Ekspertyza ppoż wprowadza alternatywne rozwiązania mające na celu spełnienie przepisów z zakresu
+        ochrony przeciwpożarowej w stosunku do rozpatrywanego budynku, obiektu budowlanego czy
+        terenu.
+        W ekspertyzie przeciwpożarowej w toku analizy przepisów, zawierającej koncepcję bezpieczeństwa,
+        właściciel obiektu otrzymuje uzgodnienie wojewódzkiego komendanta Państwowej Straży Pożarnej
+        pozwalające na bezpieczne użytkowanie budynku.`
+    },
+    {
+        flag: 'ppoz-3',
+        title: 'Uzgadnianie projektów architektoniczno-budowlanych',
+        desc: `Rodzaje obiektów, które wymagają uzgodnienia z rzeczoznawcą do spraw zabezpieczeń
+        przeciwpożarowych zostały wymienione w § 3 rozporządzenia Ministra Spraw Wewnętrznych i
+        Administracji z dnia 17 września 2021 r. w sprawie uzgadniania projektu zagospodarowania działki
+        lub terenu, projektu architektoniczno- budowlanego, projektu technicznego oraz projektu urządzenia
+        przeciwpożarowego pod względem zgodności z wymaganiami ochrony przeciwpożarowej. Celem
+        przedmiotowej współpracy jest wypracowanie założeń projektowych w zgodzie z obowiązującymi
+        przepisami techniczno – budowlanymi oraz zasadami wiedzy technicznej.`
+    },
+    {
+        flag: 'ppoz-4',
+        title: 'Nadzór ppoż. nad bezpieczeństwem pożarowym',
+        desc: `Do obowiązków właściciela lub zarządcy obiektu należy przestrzeganie przepisów
+        przeciwpożarowych, jednakże czynności określone w ustawie mogą wykonywać tylko osoby mające
+        odpowiednią wiedzę i niezbędne do tego kwalifikacje. Rzetelny nadzór nad obiektem mogą
+        sprawować zewnętrzni eksperci na podstawie stosownych ustaleń.
+        W ramach współpracy z naszymi klientami i partnerami realizujemy kompleksowy nadzór PPOŻ nad
+        bezpieczeństwem pożarowym budynku, który obejmuje:
+         okresowe i bieżące wykonywanie audytów bezpieczeństwa pożarowego;
+         przeprowadzanie szkoleń z zakresu ochrony przeciwpożarowej;
+         przeprowadzanie próbnych ewakuacji;
+         kompleksowe prowadzenie dokumentacji przeciwpożarowej firmy;
+         nadzór nad okresowymi przeglądami podręcznego sprzętu gaśniczego, hydrantów i urządzeń
+        przeciwpożarowych;
+         opracowanie oraz aktualizację Instrukcji Bezpieczeństwa Pożarowego;
+         udział w opracowaniu i weryfikację zarządzeń, regulaminów i instrukcji dotyczących ochrony
+        przeciwpożarowej;
+         współpracę z innymi instytucjami oraz właściwymi terytorialnie komendami Państwowej
+        Straży Pożarnej w zakresie ochrony przeciwpożarowej.`
+    },
+]
+
+const descAtexArray = [
+    {
+        flag: 'atex-0',
+        title: 'Ocena zagrożenia wybuchem',
+        desc: `W przypadku stosowania lub magazynowania substancji mogących tworzyć atmosfery wybuchowe z
+        powietrzem (gazy, ciecze, ciała stałe o dużym stopniu rozdrobnienia – pyły, frakcje lotne) pracodawca
+        powinien opracować dokument pt. ocena zagrożenia wybuchem.
+        Ocena zagrożenia wybuchem wymagana jest zgodnie z § 37 rozporządzenia MSWiA z dn. 7 czerwca
+        2010 r. w sprawie ochrony przeciwpożarowej budynków, innych obiektów budowlanych i terenów, i
+        obejmuje swoim zakresem:
+         wskazanie pomieszczeń zagrożonych wybuchem
+         wyznaczenie w pomieszczeniach i przestrzeniach zewnętrznych odpowiednich stref
+        zagrożenia wybuchem
+         opracowanie graficznej dokumentacji klasyfikacyjnej
+         wskazanie czynników mogących zainicjować zapłon w strefach zagrożonych wybuchem
+        Organem administracji publicznej weryfikującym wykonanie i poprawność oceny zagrożenia
+        wybuchem jest właściwy miejscowo Komendant Powiatowy/Miejski Państwowej Straży Pożarnej.`
+    },
+    {
+        flag: 'atex-1',
+        title: 'Ocena Ryzyka Wybuchu',
+        desc: `W każdym przypadku należy zapobiegać występowaniu atmosfer wybuchowych, pomocna przy tym
+        jest ocena ryzyka związanego z możliwością wystąpienia atmosfery wybuchowej.
+        
+        Pierwszym krokiem w ocenie ryzyka wybuchu jest określenie, czy niebezpieczna atmosfera
+        wybuchowa może wystąpić w danych warunkach. Jeśli ,,tak” następnie ustala się czy może dojść do
+        jej zapłonu.
+        Wymieniony proces oceny musi zawsze odnosić się do indywidualnych przypadków i nie może być
+        uogólniany. Procedura oceny musi być przeprowadzona dla każdego procesu pracy lub procesu
+        produkcyjnego.
+        Ocena ryzyka wybuchu, jest oceną całościową biorącą pod uwagę m. in.:
+         używane urządzenia do wykonywania pracy
+         cechy charakterystyczne budynków, instalacji zakładu
+         używane substancje
+         warunki pracy i procesy produkcyjne
+         ewentualne wzajemne oddziaływanie pomiędzy wymienionymi czynnikami jak również ze
+        środowiskiem pracy
+        Opracowanie oceny ryzyka wybuchu wymagane jest zgodnie z § 4 ust. 4 Rozporządzenia Ministra
+        Gospodarki z dnia 8 lipca 2010 r. w sprawie minimalnych wymagań, dotyczących bezpieczeństwa i
+        higieny pracy, związanych z możliwością wystąpienia w miejscu pracy atmosfery wybuchowej (Dz.U.
+        2010 nr 138 poz. 931).`
+    },
+    {
+        flag: 'atex-2',
+        title: 'Dokument zabezpieczenia przed wybuchem',
+        desc: `Zgodnie z § 7.1 Rozporządzenia Ministra Gospodarki z dnia 8 lipca 2010 r. w sprawie minimalnych
+        wymagań dotyczących bezpieczeństwa i higieny pracy, związanych z możliwością wystąpienia w
+        miejscu pracy atmosfery wybuchowej (Dz. U. Nr 138, poz.931), pracodawca, przed udostępnieniem
+        miejsca pracy, powinien sporządzić dokument zabezpieczenia przed wybuchem na podstawie oceny
+        ryzyka.
+        Obowiązek opracowania dokumentu zabezpieczenia przed wybuchem stawiany jest pracodawcom,
+        którzy zatrudniają pracowników na stanowiskach pracy, na których występują strefy zagrożenia
+        wybuchem. Jest on wykonywany zgodnie Rozporządzeniem Ministra Gospodarki z dnia 8 lipca 2010r.
+        w sprawie minimalnych wymagań dotyczących bezpieczeństwa i higieny pracy, związanych z
+        możliwością wystąpienia w miejscu pracy atmosfery wybuchowej (Dz. U. Nr 138, poz. 931).
+        Opracowanie Dokumentu Zabezpieczenia Przed Wybuchem dla stanowisk pracy powinno być
+        wykonane przed rozpoczęciem pracy. Ponadto powinno być poddane aktualizacji lub przeglądowi w
+        przypadku, gdy miejsce pracy, urządzenia do wykonywania pracy lub organizacja pracy zostały
+        zasadniczo zmienione, rozbudowane lub przekształcone.`
+    },
+]
+
+let subButtonsArray = [];
+
 const renderButtons = (offerId, buttonsArray) => {
     subMenu.innerHTML = '';
+    subButtonsArray = [];
+    let index = 0;
     for(const button of buttonsArray) {
         const buttonEl = document.createElement('button');
         buttonEl.textContent = button;
-        buttonEl.setAttribute('id', `${offerId}-${button.id}`);
+        buttonEl.setAttribute('id', `${offerId}-${index}`);
         buttonEl.classList = 'sub-menu-btn';
         subMenu.append(buttonEl);
+        subButtonsArray.push(buttonEl);
+        index++;
     }
+    let arrayDesc = [];
+    if(offerId === 'ppoz') {
+        arrayDesc = descPpozArray;
+    } else if(offerId === 'atex') {
+        arrayDesc = descAtexArray;
+    }
+    subTitleOpen(subButtonsArray, offerId, arrayDesc);
 }
 
-const changePhoto = (offerId) => {
+const subTitleOpen = (arrayOfButtons, offerId, arrayDesc) => {
+    arrayOfButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            modal.classList.add('modal-show');
+            if(arrayDesc[index].flag === `${offerId}-${index}`) {
+                modalContainer.innerHTML = `
+                <h1>${arrayDesc[index].title}</h1>
+                <p>${arrayDesc[index].desc}</p>
+                <button>Zamknij</button>
+                `;
+            }
+            const closeButton = modalContainer.querySelector('button');
+            closeButton.addEventListener('click', () => {
+                modal.classList.remove('modal-show');
+            })
+            modal.addEventListener('click', () => {
+                modal.classList.remove('modal-show');
+            })
+        })
+    })
+}
+
+const changeElements = (offerId) => {
     if(offerId === 'atex') {
         photoContainer.classList = '';
-
         photoContainer.className = 'atex-active';
         renderButtons(offerId, atexButtonsArray);
 
     } else if (offerId === 'seveso') {
         photoContainer.classList = '';
-
         photoContainer.className = 'seveso-active';
         renderButtons(offerId, sevesoButtonsArray);
 
     } else if (offerId === 'ppoz') { 
         photoContainer.classList = '';
-
         photoContainer.className = 'ppoz-active';
         renderButtons(offerId, ppozButtonsArray);
         
     } else {
         photoContainer.classList = '';
-
         photoContainer.className = 'training-active';
         renderButtons(offerId, trainingButtonsArray);
 
@@ -65,11 +239,11 @@ const highlitedOffer = (btn) => {
 
 function handleClick(event) {
     const btnId = event.target.id;
-    console.log(btnId);
-    changePhoto(btnId);
+    changeElements(btnId);
     highlitedOffer(event.target);
 }
 
+window.addEventListener('load', renderButtons('ppoz', ppozButtonsArray));
 ppozBtn.addEventListener('click', handleClick);
 atexBtn.addEventListener('click', handleClick);
 sevesoBtn.addEventListener('click', handleClick);
